bfdb58ed47b0bc55cff231a91b89d50c1cfa603b
Djielo <business.jlo68@gmail.com>
2022-02-04 20:55:07 +0100

    Refactoring 1

--- starterOnly/scripts/main.js
+++ starterOnly/scripts/main.js
@@ -1,28 +1,43 @@
-import { validateForm } from "./validation.js";
 import { launchModal, closeModal } from "./modal.js";
 import { editNav } from "./menu.js";
 
+//=================================================//
+//================== REGEXP =======================//
+const regexName = /^[a-zA-Z-\u00C0-\u024F]{2,}$/;
+const regexpEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,10})+$/;
+const regexDate = /^19[00-99]{2}|200[0-8]{1}-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|2[0-9]|3[0-1])$/;
+const regexNumber = /^[0-9]{1}$/;
+
+//=================================================//
+//================== CONSTANTES ===================//
+const burgerButton = document.querySelector("#burger");
+const modalCrossClose = document.querySelector(".close");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const modalbg = document.querySelector(".bground");
+const formAll = document.querySelector("#form");
+
 //=====================================================//
 //================= RESPONSIVE MENU ===================//
-const burgerButton = document.querySelector("#burger");
 burgerButton.addEventListener("click", editNav);
 
 //==================================================//
 //================ OPEN AND CLOSE MODAL ============//
-const modalCrossClose = document.querySelector(".close");
-const modalBtn = document.querySelectorAll(".modal-btn");
 modalCrossClose.addEventListener("click", closeModal);
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-//=======================================================//
-//====================== GLOBAL =========================//
-// On va chercher le formulaire dans le DOM
-export const formAll = document.querySelector("#form");
+// Open modal form by click on button
+function launchModal() {
+  modalbg.style.display = "block";
+}
 
-//========================================================//
-//======= EVENT LISTENERS + ERRORS DISPLAY ===============//
+// Close modal by cross
+function closeModal() {
+  modalbg.style.display = "none";
+}
+//======================================================================================//
+//======= DECLARATION DE FONCTION CONTENANT ADD EVENT LISTENERS + ERRORS DISPLAY =======//
 // Ecouter la modification de prénom
-export const validateFirstName = () => {
+const validateFirstName = () => {
   formAll.first.addEventListener("blur", () => {
     const inputFirstName = document.querySelector("input#first");
     const errorFirstName = document.querySelector("span.firstError");
@@ -38,15 +53,12 @@ export const validateFirstName = () => {
     }
   });
 };
-
-validateFirstName();
-
 // Ecouter la modification de nom
-export const validateLastName = () => {
+const validateLastName = () => {
   formAll.last.addEventListener("blur", () => {
     const inputLastName = document.querySelector("input#last");
     const errorLastName = document.querySelector("span.lastError");
-
+    
     // analyse la condition (longueur + regex)
     if (inputLastName.value.length > 1 && regexName.test(inputLastName.value) === true) {
       inputLastName.style.border = "3px solid green";
@@ -59,11 +71,8 @@ export const validateLastName = () => {
     }
   });
 };
-
-validateLastName();
-
 // Ecouter la modification de email
-export const validateEmail = () => {
+const validateEmail = () => {
   formAll.email.addEventListener("blur", () => {
     const inputEmail = document.querySelector("input#email");
     const errorEmail = document.querySelector("span.emailError");
@@ -79,11 +88,8 @@ export const validateEmail = () => {
     }
   });
 };
-
-validateEmail();
-
 // Ecouter la modification de birthdate
-export const validateBirthdate = () => {
+const validateBirthdate = () => {
   formAll.birthdate.addEventListener("blur", () => {
     const inputBirthdate = document.querySelector("input#birthdate");
     const errorBirthdate = document.querySelector("span.birthdateError");
@@ -99,15 +105,12 @@ export const validateBirthdate = () => {
     }
   });
 };
-
-validateBirthdate();
-
 // Ecouter la modification de quantity
-export const validateQuantity = () => {
+const validateQuantity = () => {
   formAll.quantity.addEventListener("blur", () => {
     const inputQuantity = document.querySelector("input#quantity");
     const errorQuantity = document.querySelector("span.quantityError");
-
+    
     if (regexNumber.test(inputQuantity.value)) {
       inputQuantity.style.border = "3px solid green";
       errorQuantity.style.visibility = "hidden";
@@ -119,11 +122,8 @@ export const validateQuantity = () => {
     }
   });
 };
-
-validateQuantity();
-
 // Ecouter la modification de condition
-export const validateCondition = () => {
+const validateCondition = () => {
   validateCondition.value = true;
   formAll.checkbox1.addEventListener("click", () => {
     const inputCondition = document.querySelector("input#checkbox1");
@@ -139,25 +139,46 @@ export const validateCondition = () => {
   });
 };
 
+
+//=========================== VALIDATION DES INPUTS ==============================//
+//=================== APPEL DE FONCTION DES VALIDATION D'INPUT ===================//
+validateFirstName();
+validateLastName();
+validateEmail();
+validateBirthdate();
+validateQuantity();
 validateCondition();
 
-//=================================================//
-//================== REGEXP =======================//
-const regexName = /^[a-zA-Z-\u00C0-\u024F]{2,}$/;
-const regexpEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,10})+$/;
-const regexDate = /^19[00-99]{2}|200[0-8]{1}-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|2[0-9]|3[0-1])$/;
-const regexNumber = /^[0-9]{1}$/;
 
-//================================================================//
-//=================== CONTROL CONTENT IN INPUT ===================//
-// export const validateFirstName = inputFirst;
+
+//====================================== VALIDATION DU FORMULAIRE ==================================//
+// VERIFICATION DE LA VALIDITÉ DE CHAQUE CHAMP // SI TRUE, RENVOIE VERS L'ACCEPTATION DE VALIDATION //
+const validateForm = () => {
+  if (
+    validateFirstName.value &&
+    validateLastName.value &&
+    validateEmail.value &&
+    validateBirthdate.value &&
+    validateQuantity.value &&
+    validateCondition.value
+  ) {
+    console.log("Afficher la modal de confirmation");
+  } else {
+    console.log(validateFirstName.value);
+    console.log(validateLastName.value);
+    console.log(validateEmail.value);
+    console.log(validateBirthdate.value);
+    console.log(validateQuantity.value);
+    console.log(validateCondition.value);
+  }
+};
 
 //===============================================//
 //================== SUBMIT =====================//
-// Permet d'éviter le rechargement de la page, de valider ou pas le formulaire et agir selon le résultat (affichage des erreurs ou validation du form)
-
-// Je rajoute un écouteur pour savoir à quel moment le formuaire est soumis afin de lancer le controle de validation du formulaire
+// Écouteur pour savoir à quel moment le formuaire est soumis afin de lancer le controle de validation du formulaire
 formAll.addEventListener("submit", (e) => {
+  // Permet d'éviter le rechargement de la page
   e.preventDefault();
+  // Et de valider ou pas le formulaire
   validateForm();
 });
