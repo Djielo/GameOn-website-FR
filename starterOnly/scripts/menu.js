54cccb5d1fd4ac645889744400ae940468c8d34f
Djielo <business.jlo68@gmail.com>
2022-02-07 19:41:20 +0100

    CSS OK

--- starterOnly/scripts/menu.js
+++ starterOnly/scripts/menu.js
@@ -4,10 +4,10 @@ const burgerButton = document.querySelector("#burger");
 burgerButton.addEventListener("click", editNav);
 
 function editNav() {
-    const x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+  const x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
